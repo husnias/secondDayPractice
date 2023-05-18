a6afe5e4178cb5b97c1fd8281c6ec13a9a0bbb2e
Husnia Sidiqi <hkpayenda@gmail.com>
2023-05-17 21:29:39 -0400

    make some changes

---
+++ src/tek/daytwo/Class.java
@@ -0,0 +1,7 @@
+package tek.daytwo;
+
+public class Class {
+	public static void main(String[] args) {
+		System.out.println("This Is another Practice class");
+	}
+}
--- src/tek/daytwo/PracticeClass.java
+++ src/tek/daytwo/PracticeClass.java
@@ -3,5 +3,6 @@ package tek.daytwo;
 public class PracticeClass {
 	public static void main(String[] args) {
 		System.out.println("This Is Practice class");
+		System.out.println("This Is second Practice class");
 	}
 }
